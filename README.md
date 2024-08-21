aa6acb76102a800ea59841df0d135008780f1c9e
demenciabeatsdev <demenciabeats.dev@gmail.com>
2024-08-20 23:18:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Vite + React Template
+
+Este es un template básico para iniciar un proyecto con Vite y React.
+
+## Instalación
+
+```bash
+npm install
---
+++ public/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      {/* Your App Component */}
+    </div>
+  );
+}
+
+export default App;
